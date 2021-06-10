2d387e0d51e17dbe7254ae81f12a08114e27d061
Anchenni <58232633+Anchenni@users.noreply.github.com>
2021-06-10 02:30:23 +0200

    Update cowsandbulls.py

--- Exercices/cowsandbulls.py
+++ Exercices/cowsandbulls.py
@@ -18,46 +18,48 @@ print("=========================================================================
 print(" ")
 #clear = lambda:os.system("clear")
 def random_number():
-    a = list()
-    for i in range(4):
-        a.append(random.choice('123456789'))
+    a = int(random.randint(1000,9999))
     #print(a)
     return(a)
 
 #1234
 
-a = random_number()
+
 #print(a)
 #1345
-tmp = 1;
-while True:
-    cows = 0
-    bulls = 0
+def play_game():
+    a = str(random_number())
+    tmp = 1;
     while True:
-        guess = input("Enter your guess: ")
-        if len(guess) > 4 or len(guess) < 4 or len(guess) == 0:
-            print("The number must be 4 digits long!")
-        else:
+        cows = 0
+        bulls = 0
+        while True:
+            guess = input("Enter your guess: ")
+            if len(guess) > 4 or len(guess) < 4 or len(guess) == 0:
+                print("The number must be 4 digits long!")
+            else:
+                break
+        for i in range(len(guess)):
+            if guess[i] == a[i] :
+                cows += 1
+            elif guess[i] in a:
+                bulls += 1
+        if list(guess) == a:
+            print("Bravo! You found the number after",tmp,"tries ")
             break
-    for i in range(4):
-        if guess[i] == a[i]:
-            cows += 1
-        elif guess[i] in a:
-            bulls += 1
-    if list(guess) == a:
-        print("Bravo! You found the number after",tmp,"tries ")
-        break
-    else:
-        tmp += 1
-        if cows > 1 and bulls == 0:
-            print(cows, "cows" ,bulls,"bull")
-        elif bulls > 1 and cows == 0:
-            print(cows, "cow" ,bulls,"bulls")
-        elif bulls > 1 and cows > 1:
-            print(cows, "cows" ,bulls,"bulls")
         else:
-            print(cows, "cow" ,bulls,"bull")
-    
+            tmp += 1
+            if cows > 1 and bulls == 0:
+                print("Your Have: ",cows, "cows" ,bulls,"bull")
+            elif bulls > 1 and cows == 0:
+                print("Your Have: ",cows, "cow" ,bulls,"bulls")
+            elif bulls > 1 and cows > 1:
+                print("Your Have: ",cows, "cows" ,bulls,"bulls")
+            else:
+                print("Your Have: ",cows, "cow" ,bulls,"bull")
+        
+if __name__=="__main__":
+    play_game()
 
 
 
